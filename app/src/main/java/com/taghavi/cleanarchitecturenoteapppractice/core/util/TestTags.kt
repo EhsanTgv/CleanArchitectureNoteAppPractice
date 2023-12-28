8eb42d0d3cc3758ed5c321eb590d605e526ad933
EhsanTgv <EhsanTgv@gmail.com>
2023-12-28 10:43:29 +0300

    Writing Integration ui test for order

---
+++ app/src/main/java/com/taghavi/cleanarchitecturenoteapppractice/core/util/TestTags.kt
@@ -0,0 +1,6 @@
+package com.taghavi.cleanarchitecturenoteapppractice.core.util
+
+object TestTags {
+
+    const val  ORDER_SECTION = "ORDER_SECTION"
+}
--- app/src/main/java/com/taghavi/cleanarchitecturenoteapppractice/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/taghavi/cleanarchitecturenoteapppractice/feature_note/presentation/notes/NotesScreen.kt
@@ -12,9 +12,11 @@ import androidx.compose.material.icons.filled.Sort
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.taghavi.cleanarchitecturenoteapppractice.core.util.TestTags.ORDER_SECTION
 import com.taghavi.cleanarchitecturenoteapppractice.feature_note.presentation.notes.components.NoteItem
 import com.taghavi.cleanarchitecturenoteapppractice.feature_note.presentation.notes.components.OrderSection
 import com.taghavi.cleanarchitecturenoteapppractice.feature_note.presentation.util.Screen
@@ -74,7 +76,8 @@ fun NotesScreen(
                 OrderSection(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 16.dp),
+                        .padding(vertical = 16.dp)
+                        .testTag(ORDER_SECTION),
                     noteOrder = state.noteOrder,
                     onOrderChange = {
                         viewModel.onEvent(NotesEvent.Order(it))
